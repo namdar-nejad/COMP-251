26fb1db77fe1330f89efe39ddca7011f79a66314
Namdar Kabolinejad <56203866+namdar-nejad@users.noreply.github.com>
2020-12-25 18:30:41 +0330

    Create main.java

---
+++ Assignment 1 (Hashing)/main.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.*;
+
+//  Use test cases from "https://github.com/mayam12/C251A1Tester/blob/main/main.java" to test my code.
+public class main {
+    public static void main(String[] args) {}
+}
